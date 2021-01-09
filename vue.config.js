534737841d1c374ecf0bbd9aa1e2132ed4590783
Sheep <ht159159@gmail.com>
2021-01-09 16:02:38 +0800

    新增1

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,6 @@ module.exports = {
     if (process.env.NODE_ENV === "development") {
       -config.output.filename("[name].[hash].js").end();
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/eric-project/" : "/"
 };
